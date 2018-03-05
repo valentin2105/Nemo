6835471ec8f9ed0d71fb6994a2d345b89f858236
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2018-03-05 11:21:45 +1100

    enable responsiv.

--- static/project/js/footer.js
+++ static/project/js/footer.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
 	// get current URL path and assign 'active' class
 	var pathname = window.location.pathname;
 	$('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+
+
+
+
+
 })
+
+
+function clickToto(e) {
+	e.target.innerHTML = "Toto";
+}
