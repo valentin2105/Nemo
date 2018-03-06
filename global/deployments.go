39e72b4d00baccf5a4236754a266fe8ba28eb0d6
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2018-03-06 22:19:18 +1100

    fix deployment version API change w/ k8S version

--- global/deployments.go
+++ global/deployments.go
@@ -31,12 +31,32 @@ func ListDeployments() DeploymentList {
 	}
 	var deployments appsv1.DeploymentList
 	if err := client.List(context.Background(), k8s.AllNamespaces, &deployments); err != nil {
-		logrus.Warn("Error " + err.Error())
 
 		var deployments appsv1beta1.DeploymentList
 		if err := client.List(context.Background(), k8s.AllNamespaces, &deployments); err != nil {
 			logrus.Warn("Error " + err.Error())
 		}
+
+		for _, deployments := range deployments.Items {
+			//Name
+			n := *deployments.Metadata.Name
+			nc := TrimQuotes(n)
+			// Namespace
+			ns := *deployments.Metadata.Namespace
+			nsc := TrimQuotes(ns)
+			// PodWanted
+			pw := *deployments.Status.Replicas
+			// PodRunning
+			pr := *deployments.Status.AvailableReplicas
+			st := "Ready"
+			if pw != pr {
+				st = "NotReady"
+			}
+			// Put in slice
+			d := Deployment{Status: st, Name: nc, Namespace: nsc, PodWanted: pw, PodRunning: pr}
+			dl = append(dl, d)
+		}
+		return dl
 
 	}
 	for _, deployments := range deployments.Items {
@@ -59,6 +79,7 @@ func ListDeployments() DeploymentList {
 		dl = append(dl, d)
 	}
 	return dl
+
 }
 
 // GetDeployment - describe a deployment
@@ -74,6 +95,23 @@ func GetDeployment(ns string, name string) Deployment {
 		if err := client.Get(context.Background(), ns, name, &deployment); err != nil {
 			logrus.Warn("Error " + err.Error())
 		}
+		//Name
+		n := *deployment.Metadata.Name
+		nc := TrimQuotes(n)
+		// Namespace
+		ns = *deployment.Metadata.Namespace
+		nsc := TrimQuotes(ns)
+		// PodWanted
+		pw := *deployment.Status.Replicas
+		// PodRunning
+		pr := *deployment.Status.AvailableReplicas
+		st := "Ready"
+		if pw != pr {
+			st = "NotReady"
+		}
+		// Put in slice
+		d := Deployment{Status: st, Name: nc, Namespace: nsc, PodWanted: pw, PodRunning: pr}
+		return d
 
 	}
 	//Name
@@ -93,4 +131,5 @@ func GetDeployment(ns string, name string) Deployment {
 	// Put in slice
 	d := Deployment{Status: st, Name: nc, Namespace: nsc, PodWanted: pw, PodRunning: pr}
 	return d
+
 }
